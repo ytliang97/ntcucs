b2e57ee4a194000a1c14b7b76089aa18357fad17
floatflower <zxc110888asd@gmail.com>
2018-01-29 21:37:32 +0800

    Initial commit

---
+++ src/Controller/APIv1/CategoriesController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FloatFlower.Huang
+ * Date: 2018/1/29
+ * Time: 下午 09:20
+ */
+
+namespace App\Controller\APIv1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoriesController extends Controller
+{
+    public function create(Request $request) {
+
+    }
+
+    public function edit(Request $request, $id) {
+
+    }
+
+    public function delete(Request $request, $id) {
+
+    }
+}
---
+++ src/Controller/AdminView/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FloatFlower.Huang
+ * Date: 2018/1/29
+ * Time: 下午 09:23
+ */
+
+namespace App\Controller\AdminView;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PagesController extends Controller
+{
+    public function create(Request $request) {
+
+    }
+
+    public function edit(Request $request, $id) {
+
+    }
+
+    public function listAll(Request $request, $page) {
+
+    }
+}
