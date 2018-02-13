d3e308b5eb510bbe93b85fdb7ff93c85f943b53f
floatflower <zxc110888asd@gmail.com>
2018-02-13 20:25:00 +0800

    Add security function

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FloatFlower.Huang
+ * Date: 2018/2/13
+ * Time: 下午 08:05
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends Controller
+{
+    public function login(Request $request, AuthenticationUtils $authUtils) {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
